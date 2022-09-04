4a5e732a1ca91e8af9657510caa5e768841b799a
luiscursos <47142045+luiscursos@users.noreply.github.com>
2022-09-04 09:58:49 +0200

    Update add_students.py

--- day_9/add_students.py
+++ day_9/add_students.py
@@ -3,21 +3,8 @@ import random
 
 aleatorio = random.randrange(0,100,1)
 
-# firstname = input("Enter your first name: ")
-# lastname = input("Enter your last name: ")
-# age = int(input("Enter your age: "))
-# country = input("Your country: ")
-# city = input("Your city: ")
-# street = input("Your street: ")
-# zipcode = int(input("Your zipcode: "))
-
-
-
-
-
-
-profile = {'profile':{'first_name':'a',
-                  'last_name':'a',  
+ID = {'profile':{'first name':'a',
+                  'last name':'a',  
                   'age':0,  
                    'address':{'country':'a', 
                                 'city':'a',
@@ -26,120 +13,54 @@ profile = {'profile':{'first_name':'a',
                     'scores':{'Mathematics':0,
                             'English':0,
                             'Spanish':0,
-                            'Filosofy':0,
+                            'Phylosophy':0,
                             'Physical_Education':0,
                             'Theater':0,
                             'Programming':0}
-                     }
-        }
+                            }}
                 
     
                             
-subjects = ['Mathematics', 'English', 'Spanish', 'Filosofy', 'Physical_Education', 'Theater', 'Programming' ]
-for scores in subjects:
-     user=int(input("Enter your score of "+ scores + ": "))
-     profile[scores]=user
+profile_keys = ID['profile'].keys()
+address_keys = ID['profile']['address'].keys()
+score_keys = ID['profile']['scores'].keys()
+score_values = ID['profile']['scores'].values()
+total_keys =len(profile_keys) + len(address_keys) + len(score_keys)
 
-print("The profile length is:",len(profile))
-ID = print("Your ID is: ",aleatorio)
+count = 0   
+for data_profile in ID['profile'].keys():
+    if count < 3:
+        user = input("Enter your " + data_profile + " : ")
+        ID['profile'][data_profile] = user
+        count += 1
 
-print("The profile is:", profile)
+    elif count == 3:
+        for data_address in ID['profile']['address'].keys():
+            user = input("Enter your " + data_address + " : ")
+            ID['profile']['address'][data_address] = user
+            count += 1
 
+    elif (count > 3 and count < total_keys-1):
+        for data_scores in ID['profile']['scores'].keys():
+            user = int(input("Enter your score in " + data_scores + (" : ")))
+            ID['profile']['scores'][data_scores] = user
+            count += 1
 
-
-
-
-
-print("[<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  DATOS  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]")
-
-'''
-
-ID : Dictionary
-Profile : Dictionary
-firstname : item String
-lastname : item string
-age : item integer
-address : Dictionary
-country : item string
-city : item string 
-street : item string
-zipcode : item string
-
-scores : Dictionary
-Maths : item integer
-English : item integer
-Spanish : item integer
-Filosofy : item integer
-GyM : item integer
-Theatre : item integer
-Programming : item integer
+    else:
+        print("Ron da error")
 
 
-# Maths = int(input("Enter your score of {}")).format(subjects)
-# English = int(input("Enter your score of Maths"))
-# Spanish = int(input("Enter your score of Maths"))
-# Filosofy = int(input("Enter your score of Maths"))
-# # GyM = int(input("Enter your score of Maths"))
-# # Theatre = int(input("Enter your score of Maths"))
-# # Programming = int(input("Enter your score of Maths"))
+for subjects, score in ID['profile']['scores'].items():
+    if score > 90 and score < 100:
+        print("In {} obtuvo {} is A".format(subjects, score))
+    if score > 70 and score < 89:
+        print("In {} obtuvo {} is B".format(subjects, score))
+    if score > 60 and score < 69:
+        print("In {} obtuvo {} is C".format(subjects, score))
+    if score > 50 and score < 59:
+        print("In {} obtuvo {} is D".format(subjects, score))
+    if score > 0 and score < 49:
+        print("In {} obtuvo {} is F".format(subjects, score))
 
 
-
-
-1- Create a new profile
-	1-Yes / 2-No
-	
-1.2 (NO) Modify (options)
-
-
-  Access for ID  
-                   _first name
-	_ 1 - profile /_ last name
-	|     (name)  \_ age
-	|	     |         _ country
-	|	     |_address/_ city			
-   ID                 \_street    
-	|   		       \_zipcode
-    |
-    |              __ Maths
-	|             |
-	|             |_ English
-	|             |
-	|	          |_ Spanish	
-	|_ 2 - scores_| 
-                  |_ Filosofy
-		          |
-		          |_ GyM
-    	          |
-                  |_ Theatre
-                  |
-                  |_ Programming		
-
-
-1.1 (YES) Create a new profile whit a new random ID
-
-
-
-  NEW ID       _first name
-	_ 1 - profile /_ last name
-	|     (name)  \_ age
-	|	     |         _ country
-	|	     |_address/_ city			
-   ID                 \_street    
-			           \_zipcode
-    |
-    |                 __ Maths
-	|                 |
-	|                 |_ English
-	|             |
-	|	          |_ Spanish	
-	|_ 2 - scores_| 
-                  |_ Filosofy
-		          |
-		          |_ GyM
-    	          |
-                  |_ Theatre
-                  |
-                  |_ Programming	
-'''
-
+print("Este es el perfil que acabas de crear: ",ID)
